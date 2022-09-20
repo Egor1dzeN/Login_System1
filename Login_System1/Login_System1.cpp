d70c25273afdc7c3ba023197de038edd11975fd1
Egor1dzeN <egormorozov123450@gmail.com>
2022-09-20 12:14:50 +0300

    some Change

--- Login_System1/Login_System1.cpp
+++ Login_System1/Login_System1.cpp
@@ -1,20 +1,126 @@
-﻿// Login_System1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <locale.h>
+#include<fstream>
+#include<string>
+#include<stdio.h>
+#include <cstdlib>
+#include<Windows.h>
 
-#include <iostream>
+HWND myConsole = GetConsoleWindow();
+bool checkingForSimilarity(std::string str1) {
+	using namespace std;
+	ifstream file("C:\\dataBase.txt");
+	string str2,str3;
+	while (getline(file, str2)) {
+		int i = 0;
+		for (i; str2[i] != ' '; i++) {
+			str3 = str3 + str2[i];
+		}
+		
+		if (str1 == str3) {
+			return true;
+			break;
+		}
+		str3 = ""; str2 = "";
+	}
+	return false;
+}
+bool IsLoggedIN() {
+	using namespace std;
+	string username, password, phoneNumber, un, pw, ph, nameOfFile,fullstr;
+	int firstprobel;
+	printf("Введите имя:  "); cin >> username;
+	printf("Введите пароль:  "); cin >> password;
+	nameOfFile = "C:\\dataBase.txt";
+	ifstream read(nameOfFile);
+	while (getline(read, fullstr)) {
+		un = ""; pw = ""; firstprobel = 0;
+		for (int i = 0; fullstr[i] != ' '; i++) {
+			un = un + fullstr[i];
+			firstprobel = i;
+		}
+		firstprobel += 2;
+		for (int i = firstprobel; fullstr[i] != ' '; i++) {
+			pw = pw + fullstr[i];
+		}
 
+		if (un == username && pw == password) {
+			return true;
+		}
+	}	return false;
+}
+bool checkingPhoneNumber(std::string phoneNumber) {
+	using namespace std;
+	for (int i = 0; i < phoneNumber.length(); i++)
+	{
+		if (int(phoneNumber[i]) <= 47 || int(phoneNumber[i]) >= 59)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+bool registration() {
+	std::string username1, password1,phoneNumber1;
+	printf("Введите ваш логин: ");
+	std::cin >> username1;
+	printf("Введите ваш пароль: ");
+	std::cin >> password1;
+	printf("Введите ваш телефон: ");
+	std::cin >> phoneNumber1;
+	if (checkingForSimilarity(username1) == false) {
+		if (checkingPhoneNumber(phoneNumber1) == true) {
+			std::ofstream out("C:\\dataBase.txt", std::ios::app);
+			if (out.is_open()) {
+				printf("\n");
+				out << username1 + " " + password1 + " " + phoneNumber1 << std::endl;
+			}
+			out.close();
+			return true;
+		}
+		else
+		{
+			printf("Вы ввели неправильный номер телефона");
+			return false;
+		}
+	}
+	else 
+	{
+		printf("Такой пользователь уже зарегестрирован");
+		return false;
+	}
+	return true;
+}
 int main()
 {
-    std::cout << "Hello World! Привет\n";
+	ShowWindow(myConsole, 1);
+	using namespace std;
+	int choise;
+	string username,password,str;
+    setlocale(LC_ALL, "");
+	printf("Выберите,что хотите сделать\n 1. Войти в личный кабинет.\n 2. Зарегестрироваться.\n 3. Выход из программы.\n");
+	cin >> choise;
+	switch (choise) {
+	case 1: 
+		if (IsLoggedIN() == true) {
+			printf("Вы зашли в личный кабинет");
+		}
+		else
+		{
+			printf("Неправильный логин или пароль");
+		}
+		break;
+	case 2:
+		if (registration() == true) {
+			printf("Успешно зарегестрированы");
+		}
+		break;
+	case 3: 
+		ShowWindow(myConsole, 0); 
+		break;
+	default:
+		printf("Введите коректное число...\n");
+		main(); break;
+	}
+	
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
